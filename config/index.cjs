795815b00f1ea19e8979a8876b819acb7aa7ec2c
junjayW <52025344+junjayW@users.noreply.github.com>
2024-07-30 20:45:42 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oa1Xr6AkoJlEaUxYyPGxRz0ega-s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'eUIBH_CM5D5saNnxBPAUevj-FSUCAhgtbtlIBRfQd_o',
+      useTemplateId: 'P-NFetex-0Tl6FYLlFhbVeLvVagUga6U3NnZzCjcQ34',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-31',
       festivals: [
@@ -81,13 +81,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'Femp-BVn6mfkzlKijbrUKBjFbnxXOdBMAPsHQ513t1A',
+  CALLBACK_TEMPLATE_ID: '',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oa1Xr6AkoJlEaUxYyPGxRz0ega-s',
+      id: '',
     }
   ],
 
