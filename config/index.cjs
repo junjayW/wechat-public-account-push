e9253842c03cadbc98e6bec6283979a780fa7b23
junjayW <52025344+junjayW@users.noreply.github.com>
2024-07-31 10:29:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oa1Xr6AkoJlEaUxYyPGxRz0ega-s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'dSE4_aAeZUQBF03aw_EnRiRNi3vASmRNVxvMA4y7V9k	',
+      useTemplateId: 'dSE4_aAeZUQBF03aw_EnRiRNi3vASmRNVxvMA4y7V9k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-31',
       festivals: [
