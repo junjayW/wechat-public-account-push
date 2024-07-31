8dc240e78b27994c665fad719a3946336a0cd78c
junjayW <52025344+junjayW@users.noreply.github.com>
2024-07-31 09:52:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oa1Xr6AkoJlEaUxYyPGxRz0ega-s',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '8JZd9EzE7eeq6w-teMsltYQ7PsjOjYMBFXBuZUw_U9g',
+      useTemplateId: 'sYNh9zVJVjRwPpfa0lfR9uwKQcVrl-LVg44zUAPYQgk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-31',
       festivals: [
@@ -33,11 +33,8 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '林琳', year: '1994', date: '12-31',
+          type: '生日', name: '宝贝', year: '1994', date: '12-31',
         },
-        // {
-        //   type: '节日', name: '相识纪念日', year: '2022', date: '2-15',
-        // },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
